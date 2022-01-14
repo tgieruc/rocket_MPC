70fde488713b5e0f0c4d7fc718de8e2ecababa13
Isione <isione.bonvalot@epfl.ch>
2022-01-14 22:19:58 +0100

    Added "close all and clc" at the beginning of part 2

--- src/part2.m
+++ src/part2.m
@@ -1,6 +1,9 @@
+clc;
+close all;
+
 Ts = 1/20;
 rocket = Rocket(Ts);
 [xs, us] = rocket.trim() % Compute steady−state for which 0 = f(xs,us)
-sys = rocket.linearize(xs, us)
+sys = rocket.linearize(xs, us) % Linearize the nonlinear model about trim point
 
 [sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us)
