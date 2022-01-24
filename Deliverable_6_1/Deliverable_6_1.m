dfa0098d4bd238218e06641b9c2c6fedd776b88d
Malika In-Albon <malika.in-albon@epfl.ch>
2022-01-24 18:23:12 +0100

    Began 6.1

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -1,3 +1,17 @@
 addpath(fullfile('..', 'src'));
 
 %% TODO: This file should produce all the plots for the deliverable
+Ts = 1/10; % Note that we choose a larger Ts here to speed up the simulation
+rocket = Rocket(Ts);
+H = 4
+nmpc = NMPC_Control(rocket, H);
+
+% MPC reference with default maximum roll = 15 deg
+Tf = 30;
+ref = @(t , x_) rocket.MPC_ref(t , Tf);
+
+% MPC reference with specified maximum roll = 50 deg
+roll max = deg2rad(50);
+ref = @(t , x_) rocket.MPC_ref(t , Tf, roll max);
+[T, X, U, Ref] = rocket.simulate_f(x0, Tf, nmpc, ref);
+
