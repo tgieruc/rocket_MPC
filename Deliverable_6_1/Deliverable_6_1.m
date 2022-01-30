a591ba08c928b8ace80505c1251591a8340e18b0
Isione <isione.bonvalot@epfl.ch>
2022-01-30 23:49:23 +0100

    H=4 and comparison fig NMPC

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -3,7 +3,7 @@ addpath(fullfile('..', 'src'));
 %% TODO: This file should produce all the plots for the deliverable
 Ts = 1/10; % Note that we choose a larger Ts here to speed up the simulation
 rocket = Rocket(Ts);
-H = 1;
+H = 4;
 nmpc = NMPC_Control(rocket, H);
 
 % MPC reference with default maximum roll = 15 deg
@@ -21,9 +21,15 @@ ph.fig.Name = 'NMPC_15'; % Set a figure title
 % % MPC reference with specified maximum roll = 50 deg
 roll_max = deg2rad(50);
 ref = @(t_, x_) rocket.MPC_ref(t_, Tf, roll_max);
-[T, X_50, U_15, Ref_15] = rocket.simulate_f(x0, Simul_time, nmpc, ref);
+[T, X_50, U_50, Ref_50] = rocket.simulate_f(x0, Simul_time, nmpc, ref);
 
 % Plot pose
 rocket.anim_rate = 10; % Increase this to make the animation faster
-ph = rocket.plotvis(T, X_50, U_15, Ref_15);
+ph = rocket.plotvis(T, X_50, U_50, Ref_50);
 ph.fig.Name = 'NMPC_50'; % Set a figure title
+
+%% Comparison
+figure
+plot(T,U_15(4,:),T,U_50(4,:))
+legend('15°', '50°')
+ylabel('Pdiff (%)')
